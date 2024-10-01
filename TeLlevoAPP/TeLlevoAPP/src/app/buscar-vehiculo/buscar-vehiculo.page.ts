bf4f8d5dd66758c3df7f2b91be2961ba4420ac17
Inti <inti.escobar.farias@gmail.com>
2024-10-01 16:16:45 -0300

    primer commit y proyecto arreglado con su peso

---
+++ TeLlevoAPP/TeLlevoAPP/src/app/buscar-vehiculo/buscar-vehiculo.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buscar-vehiculo',
+  templateUrl: './buscar-vehiculo.page.html',
+  styleUrls: ['./buscar-vehiculo.page.scss'],
+})
+export class BuscarVehiculoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
